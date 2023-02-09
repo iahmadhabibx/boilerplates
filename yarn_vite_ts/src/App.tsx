004560105dfe4b18614f7f01ff992b34435c1a44
Ahmad Habib <iahmadhabibx@gmail.com>
2023-02-09 12:34:17 +0500

    Boilerplate added for Yarn + Vite + React + Scss + Ts

---
+++ yarn_vite_ts/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+     Boiler Plate By <a href="https://github.com/iahmadhabibx" target="_blank">Ahmad Habib</a>
+    </div>
+  )
+}
+
+export default App
