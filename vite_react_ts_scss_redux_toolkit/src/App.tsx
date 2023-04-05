7aab1cfc1892a56214ef660d2540c6e21df945ae
Ahmad Habib <49106201+iahmadhabibx@users.noreply.github.com>
2023-04-05 13:43:16 +0500

    Boiler Plate for Vite ReactJS TS, Scss, Redux

---
+++ vite_react_ts_scss_redux_toolkit/src/App.tsx
@@ -0,0 +1,7 @@
+import RouterModule from "./modules/Router/router.module";
+
+function App() {
+  return <RouterModule />
+}
+
+export default App
---
+++ vite_react_ts_scss_redux_toolkit/src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store, { persistor } from "./redux-toolkit/store/store";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import "./index.scss";
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+)
